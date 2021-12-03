bbc877824e9168e0684e0ea739a1ab43d20e62b6
Ville Vik <villevik18@gmail.com>
2021-12-03 14:41:22 +0100

    FootballModel.js and removed duplicates

---
+++ src/js/FootballModel.js
@@ -0,0 +1,13 @@
+class FootballModel{
+    constructor(){
+        this.upVoted = [];
+        this.popular = [];
+    }
+    addUpVote(id){}
+    removeUpVote(id){}
+    addDownVote(id){}
+    removeDownVote(id){}
+    addToPopular(id){}
+    removeFromPopular(id){}
+
+}
