3cedd7c83a0a2de0f5604727ca32a8458cf369c6
Ville Vik <villevik18@gmail.com>
2021-12-05 14:21:22 +0100

    addHighlights in FootballModel

--- src/js/FootballModel.js
+++ src/js/FootballModel.js
@@ -1,8 +1,23 @@
+import HighlightSource from "./apiSource/highlightSource";
 class FootballModel{
     constructor(){
+        this.highlights = [];
+        this.testValue = 5;
         this.upVoted = [];
         this.popular = [];
+        this.addHighlights();
     }
+    addHighlights(hlights){
+        //this.highlights.push(data);
+        //this.highlights = null;
+        //this.highlights = [...this.highlights,"Hello"];
+        HighlightSource.getHighlight().then(data => {this.highlights = data; console.log(data);});
+        console.log(this.highlights);
+    }
+    getHighlights(){
+        return this.highlights;
+    }
+
     addUpVote(id){}
     removeUpVote(id){}
     addDownVote(id){}
@@ -11,3 +26,4 @@ class FootballModel{
     removeFromPopular(id){}
 
 }
+export default FootballModel;
