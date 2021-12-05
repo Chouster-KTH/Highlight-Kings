e18a485f6b6c7ce2293e7dc40a55dbafdf22b8e5
Ville Vik <villevik18@gmail.com>
2021-12-05 14:57:20 +0100

    FootballModel cleanup and react-router-dom

--- src/js/FootballModel.js
+++ src/js/FootballModel.js
@@ -2,20 +2,13 @@ import HighlightSource from "./apiSource/highlightSource";
 class FootballModel{
     constructor(){
         this.highlights = [];
-        this.testValue = 5;
         this.upVoted = [];
         this.popular = [];
         this.addHighlights();
     }
-    addHighlights(hlights){
-        //this.highlights.push(data);
-        //this.highlights = null;
-        //this.highlights = [...this.highlights,"Hello"];
+    addHighlights(){
         HighlightSource.getHighlight().then(data => {this.highlights = data; console.log(data);});
         console.log(this.highlights);
-    }
-    getHighlights(){
-        return this.highlights;
     }
 
     addUpVote(id){}
