23a3678e432507717026500780b3a0cfbdf20df5
Ville Vik <villevik18@gmail.com>
2021-12-06 14:54:07 +0100

    fixed https in matchSource

--- src/js/apiSource/matchSource.js
+++ src/js/apiSource/matchSource.js
@@ -1,6 +1,6 @@
 const MatchSource =  {
     async compCall(params) {
-        let response = await fetch("http://api.football-data.org/v2/competitions/" +params, {
+        let response = await fetch("https://api.football-data.org/v2/competitions/" +params, {
             "method": "GET",              // HTTP method
             "headers": {                  // HTTP headers
                 'X-Auth-Token': "108695e7a2ba4fe6982714e3c844c10a"
