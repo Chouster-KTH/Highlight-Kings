5e149e5790fc44ab814dce6162480101c5be28b2
nnayeri <nnayeri@kth.se>
2021-12-09 11:42:54 +0100

    VERY FUNNY JOKE HA HA

--- src/js/aboutUsView.js
+++ src/js/aboutUsView.js
@@ -11,7 +11,7 @@ function AboutUs(props) {
             <h1 className="aboutUs-content">
                 About us</h1>
 
-            <div type='text' className='b'>This website is a project done by Ville Vik, Carl Johan Jonsson, Nina Nayeri and Daniel Chouster as part of the DH2642 course at KTH Royale Institute of Technology.
+            <div type='text' className='b'>This website is a project done by Ville Vik, Carl Johan Jonsson, Nima Nayeri and Daniel Chouster as part of the DH2642 course at KTH Royale Institute of Technology.
                 <br></br><br></br>Feel free to contact team leader Ville Vik at <a href="mailto:vvik@kth.se">vvik@kth.se</a> for feedback or business inquiries.
 
             </div>
