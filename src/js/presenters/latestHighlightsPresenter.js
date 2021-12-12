e887d64511ed51da4c0a8fd4ff3e4d0c2974f7bc
Ville Vik <villevik18@gmail.com>
2021-12-12 16:40:41 +0100

    Added latesthighlight presenter

---
+++ src/js/presenters/latestHighlightsPresenter.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import HighlightSource from '../apiSource/highlightSource';
+import LatestHighlights from '../latestHighlightsView';
+
+
+function LatestHighlightsPresenter(props){
+  const [hLight, setHLight] = useState(null);
+
+  useEffect(()=>{
+    async function getHLight(){
+      let data = await HighlightSource.getHighlight();
+      setHLight(data);
+    }
+    getHLight()
+  }, [])
+  
+  
+
+  return (<React.Fragment>
+    {hLight && <LatestHighlights 
+    highlights={hLight} />}
+    </React.Fragment>);
+}
+
+export default LatestHighlightsPresenter;
