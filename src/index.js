21e9a78243fa5e4abc1d88d8351694bc27f7a43d
Ville Vik <villevik18@gmail.com>
2021-12-19 19:13:56 +0100

    Removed unused code

--- src/index.js
+++ src/index.js
@@ -3,25 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import MatchSource from './js/apiSource/matchSource';
-import HighlightSource from './js/apiSource/highlightSource.js';
-//import LatestHighlights from './js/latestHighlightsView'
 import FootballModel from './js/FootballModel';
-import {initializeApp} from "firebase/app";
-import firebase from 'firebase/compat/app';
 import Firebase from './Firebase/firebase';
 import persistModel from './Firebase/firebaseModel';
 
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDVtpnoUGoVYTebaMPbD7N21XXqL5ibsTM",
-  authDomain: "highlightkings-1d6b6.firebaseapp.com",
-  databaseURL: "https://highlightkings-1d6b6-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "highlightkings-1d6b6",
-  storageBucket: "highlightkings-1d6b6.appspot.com",
-  messagingSenderId: "182355394077",
-  appId: "1:182355394077:web:d18296256369e1047f90f4"
-}
-//firebase.initializeApp(firebaseConfig);*/
 const fb = new Firebase();
 const footballModel = new FootballModel();
 persistModel(footballModel);
