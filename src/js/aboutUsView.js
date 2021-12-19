724451247b1420bb950d48d3174d87869fb52e80
Ville Vik <villevik18@gmail.com>
2021-12-19 23:55:37 +0100

    Fixed conflicting css

--- src/js/aboutUsView.js
+++ src/js/aboutUsView.js
@@ -10,7 +10,7 @@ function AboutUs(props) {
             <h1 className="aboutUs-content">
                 About Us</h1>
 
-            <div type='text' className='infoText'>This website is a project done by Ville Vik, Carl Johan Jonsson, Nima Nayeri and Daniel Chouster as part of the DH2642 course at KTH Royale Institute of Technology.
+            <div type='text' className='auText'>This website is a project done by Ville Vik, Carl Johan Jonsson, Nima Nayeri and Daniel Chouster as part of the DH2642 course at KTH Royale Institute of Technology.
                 <br></br><br></br>Feel free to contact team leader Ville Vik at <a href="mailto:vvik@kth.se">vvik@kth.se</a> for feedback or business inquiries.
 
                 <h1 className="aboutUs2-content">
